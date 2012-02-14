504d472b63cbce38b7368fd2b700bda4d2713169
delphi2sharp <tim@today.co.nz>
2012-02-14 14:50:01 +1300

    first commit

---
+++ CRC16Test/CRC16Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CRC16Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte[] data = UTF8Encoding.UTF8.GetBytes("Testing");
+
+            CRC16 crc16 = new CRC16();
+
+            byte[] checksum;
+
+            checksum = crc16.ComputeChecksumBytes(data);
+
+            string hex = BitConverter.ToInt16(checksum, 0).ToString("X");
+
+            Console.WriteLine(hex);
+            Console.WriteLine(checksum[0]);
+            Console.WriteLine(checksum[1]);
+            
+            //outputs:
+            //9DBF    -- which matches http://www.lammertbies.nl/comm/info/crc-calculation.html
+            //191
+            //157
+
+            Thread.Sleep(100000);
+
+        }
+    }
+}
---
+++ DelphiCRC16Test/Unit1.dfm
@@ -0,0 +1,25 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 286
+  ClientWidth = 426
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 144
+    Top = 96
+    Width = 137
+    Height = 73
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
